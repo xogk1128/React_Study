0e01d3bab75aa825cc0dbde3ced6abd33a185577
xogk1128 <xogk1128@naver.com>
2023-12-06 19:34:14 +0900

    특정 요소 삭제하기

--- state-demo/src/EmojiClicker.jsx
+++ state-demo/src/EmojiClicker.jsx
@@ -1,15 +1,31 @@
 import { v4 as uuid } from "uuid";
 import { useState } from "react";
 
+function randomEmoji() {
+    const choices = ["⭐️", "🍞", "🐶", "🕋"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
 const EmojiClicker = () => {
     const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😭" }]);
     const addEmoji = () => {
-        setEmojis([...emojis, { id: uuid(), emoji: "😄" }]);
+        setEmojis([...emojis, { id: uuid(), emoji: randomEmoji() }]);
+    };
+
+    const deleteEmoji = (id) => {
+        console.log(id);
+        setEmojis((prevEmojis) => {
+            return prevEmojis.filter((e) => e.id !== id);
+        });
     };
     return (
         <div>
             {emojis.map((e) => (
-                <span key={e.id} style={{ fontSize: "4rem" }}>
+                <span
+                    onClick={() => deleteEmoji(e.id)}
+                    key={e.id}
+                    style={{ fontSize: "4rem" }}
+                >
                     {e.emoji}
                 </span>
             ))}
