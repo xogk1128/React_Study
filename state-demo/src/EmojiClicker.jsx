cf68f65e1b069403044f89ddf95f5622343f5aa5
xogk1128 <xogk1128@naver.com>
2023-12-06 19:23:24 +0900

    배열 요소에 key 추가

--- state-demo/src/EmojiClicker.jsx
+++ state-demo/src/EmojiClicker.jsx
@@ -1,14 +1,17 @@
+import { v4 as uuid } from "uuid";
 import { useState } from "react";
 
 const EmojiClicker = () => {
-    const [emojis, setEmojis] = useState(["😭"]);
+    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😭" }]);
     const addEmoji = () => {
-        setEmojis([...emojis, "😄"]);
+        setEmojis([...emojis, { id: uuid(), emoji: "😄" }]);
     };
     return (
         <div>
             {emojis.map((e) => (
-                <span style={{ fontSize: "4rem" }}>{e}</span>
+                <span key={e.id} style={{ fontSize: "4rem" }}>
+                    {e.emoji}
+                </span>
             ))}
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
